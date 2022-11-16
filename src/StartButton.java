1d75debfd970fdd9fb3b4268b8d34baaf50bf5e2
Eui-seong <tlsdmltjd52@gamil.com>
2022-11-16 20:26:33 +0900

    Eui-seong

--- src/StartButton.java
+++ src/StartButton.java
@@ -12,7 +12,7 @@ public class StartButton extends JButton implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e){
-        System.out.println("HAHA");
+        System.out.println("Game Start");
         new Pacman().Start();
         Frame.dispose();
     }
