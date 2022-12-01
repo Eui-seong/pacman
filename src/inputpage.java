de65961be94cd636b948fcd31b686b8db1121a6d
Eui-seong <tlsdmltjd52@gamil.com>
2022-12-01 17:01:37 +0900

    set inputpage's background color(Black)

--- src/inputpage.java
+++ src/inputpage.java
@@ -3,19 +3,24 @@ import java.awt.*;
 
 public class inputpage extends JFrame {
     public inputpage(int p){
+        getContentPane().setBackground(Color.BLACK);
+
         setLayout(null);
         JLabel game_over = new JLabel("GAME OVER");
         game_over.setFont(new Font("Serif",Font.BOLD,30));
+        game_over.setForeground(Color.WHITE);
         game_over.setBounds(48,10,200,50);
         add(game_over);
 
         JLabel output_point = new JLabel("YOUR POINT : "+p);
         output_point.setFont(new Font("Serif",Font.BOLD,20));
+        output_point.setForeground(Color.WHITE);
         output_point.setBounds(62,50,200,50);
         add(output_point);
 
         JLabel what_name = new JLabel("WHAT IS YOUR NAME?");
         what_name.setFont(new Font("Serif",Font.BOLD,15));
+        what_name.setForeground(Color.WHITE);
         what_name.setBounds(60,80,200,50);
         add(what_name);
 
